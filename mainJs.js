7f11aae677165ef8ebb8897c95c347fe91f285fc
Sven Kilberg <sven.kilberg@gmail.com>
2020-08-27 09:03:20 +0200

    Logic for removing tiles added. Not successfull

--- mainJs.js
+++ mainJs.js
@@ -13,6 +13,7 @@ const GameBoardTile = function() {
     this.yPoint;
     this.width;
     this.height;
+    this.hasMarker;
 }
 
 
@@ -44,6 +45,7 @@ for (let i = 0; i < 6; i++){
         tiles[tilesCount].yPoint = startRectYPoint + (i * rectHeight)
         tiles[tilesCount].width = rectWidth;
         tiles[tilesCount].height = rectHeight;
+        tiles[tilesCount].hasMarker = false;
 
         tilesCount++       
     }
@@ -75,11 +77,19 @@ function onClick(e) {
     // context.arc(x,y,r,sAngle,eAngle,counterclockwise); To create a circle with arc(): Set start angle to 0 and end angle to 2*Math.PI.
     tiles.forEach(element => {
         console.log(e.x, e.y);
-        if (e.x > element.xPoint && e.x < element.xPoint + element.width && e.y > element.yPoint && e.y < element.yPoint + element.height) {
+        console.log(element.hasMarker);
+        if (e.x > element.xPoint && e.x < element.xPoint + element.width && e.y > element.yPoint && e.y < element.yPoint + element.height && !element.hasMarker) {
             ctx.beginPath();
             ctx.arc(element.xPoint + (element.width / 2), element.yPoint + (element.height / 2), element.width / 2 - 4 , 0, 2 * Math.PI);
             ctx.fillStyle = "red";
             ctx.fill();
+        }
+        else {
+            ctx.beginPath();
+            ctx.rect(element.xPoint + 8, element.yPoint + 8, element.width - 8, element.height - 8);
+            ctx.fillStyle = "white";
+            ctx.fill();
+
         }
     });
 }
